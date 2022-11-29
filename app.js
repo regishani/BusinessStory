c5020389d4a05d65733eba581e4bd3a2a20bec00
Regina Zhyldyzbekova <regina.zhyldyzbekova@gmail.com>
2022-11-29 20:19:26 +0600

    first commit

---
+++ app.js
@@ -0,0 +1,35 @@
+const express = require ('express')
+const dotenv = require ('dotenv')
+const mongoose = require("mongoose")
+const morgan = require ('morgan')
+const exphbs = require('express-handlebars')
+const connectDB = require ('./config/db')
+
+dotenv.config =({path:'./config/config.env' })
+connectDB()
+const app = express()
+// logging
+if(process.env.NODE_ENV === "development"){
+    app.use(morgan('dev'))
+}
+
+// handlebars
+
+app.engine(
+    '.hbs',
+    exphbs.engine({
+      defaultLayout: 'main',
+      extname: '.hbs',
+    })
+  )
+  app.set('view engine', '.hbs')
+  
+//router
+
+app.use('/', require('./routes/index'))
+
+const PORT = process.env.PORT || 3000
+app.listen (
+    PORT,
+    console.log(`Server is runningin ${process.env.NODE.ENV} mode on ${PORT}`)
+)
---
+++ routes/index.js
@@ -0,0 +1,13 @@
+const { application } = require('express')
+const express = require ('express')
+const router = express.Router()
+
+router.get('/', (req, res) =>{
+    res.send('Login')
+})
+
+router.get('/dashboard', (req, res)=>{
+    res.send('Dashboard')
+})
+
+module.exports = router
